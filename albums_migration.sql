b79cf3c345cef63a26d1206039cafcd8300cb291
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-23 16:42:49 -0500

    completed the tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(255),
+    album_name VARCHAR(255),
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(255),
+    PRIMARY KEY (id)
+);
