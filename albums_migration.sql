e8ab42e68e3995d393db6733b566da8b170e9ef6
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-24 13:12:11 -0500

    completed part 1 of basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(255),
     album_name VARCHAR(255),
-    release_date DATE,
+    release_date INT,
     sales FLOAT,
     genre VARCHAR(255),
     PRIMARY KEY (id)
