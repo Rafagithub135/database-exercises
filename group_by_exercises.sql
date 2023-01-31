d1b5946e7d6d6959e4c91a9c0eb6c65bbf322650
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-31 16:01:50 -0500

    working on database design exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,20 +6,20 @@ SELECT DISTINCT title FROM titles;
 
 -- 2. Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
 
-SELECT last_name FROM employees GROUP BY last_name HAVING last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY last_name ORDER BY last_name;
 
 -- 3. Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 
-SELECT first_name, last_name FROM employees GROUP BY first_name, last_name HAVING last_name LIKE 'E%' AND last_name LIKE '%E';  -- NOTE TO SELF:  Can use
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY first_name, last_name;
 
 -- 4. Find the unique last names with a 'q' but not 'qu'.
 
-SELECT last_name FROM employees GROUP BY last_name HAVING last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 -- 5. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
 
-SELECT last_name, COUNT(last_name) FROM employees GROUP BY last_name HAVING last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 -- 6. Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
 
-SELECT COUNT(*), gender FROM employees  WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' GROUP BY gender;
+SELECT gender, COUNT(*) FROM employees  WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' GROUP BY gender;
