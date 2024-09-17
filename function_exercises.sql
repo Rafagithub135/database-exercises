9a68531e0b655b2489322a684a96f0c76ac904df
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-09-17 16:37:47 -0400

    edited exercises for database entry

--- function_exercises.sql
+++ function_exercises.sql
@@ -1,8 +1,8 @@
 USE employees;
 
-SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';  -- Since we're looking for a name with the first and last letters being the same, we can use E%E and just use one "where" statement.
 
-SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
+SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25; -- Can also use "Where birth_date LIKE '12-25'".
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND month(birth_date) = 12 AND day(birth_date) = 25;
 
