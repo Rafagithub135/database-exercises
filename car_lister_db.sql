393cf4dec9584d8beee64ca980731e7fe61e31cf
rafagithub135 <mccloskey.ralph35@gmail.com>
2026-04-09 11:29:15 -0400

    updating files for database design

--- car_lister_db.sql
+++ car_lister_db.sql
@@ -44,8 +44,7 @@ CREATE TABLE categories
     hatchback VARCHAR(255),
     mini_van VARCHAR(255)
 );
-
--- Create a join table "ad_categories" to store the relationships between ads and categories:
+#   - Create a join table "ad_categories" to store the relationships between ads and categories:
 CREATE TABLE ad_categories
 (
     ad_id       INT,
@@ -70,4 +69,3 @@ VALUES ('John', 'Doe', 'johndoe@example.com', 'secretpassword');
 
 INSERT INTO ads (user_id, title, make, model, year, category)
 VALUES (1, '1969 Mercury Cougar XR-7 390GT', 'Mercury', 'Cougar XR - 7', '1969', 'sedan');
-
